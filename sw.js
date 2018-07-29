cd3f054d9259458115617f18bf4ddbc03299836a
Valer-Muresan <muresanvaler86@yahoo.com>
2018-07-29 23:01:56 +0300

    fix the fetch error

--- sw.js
+++ sw.js
@@ -1,29 +1,28 @@
 var staticCacheName = 'v1-restaurant';
-var allCache = [
-  './',
-  './restaurant.html',
-  './index.html',
-  './js/restaurant_info.js',
-  './js/main.js',
-  './js/dbhelper.js',
-  './img/1.jpg',
-  './img/2.jpg',
-  './img/3.jpg',
-  './img/4.jpg',
-  './img/5.jpg',
-  './img/6.jpg',
-  './img/7.jpg',
-  './img/8.jpg',
-  './img/9.jpg',
-  './img/10.jpg',
-  './data/restaurants.json',
-  './css/styles.css',
-];
 self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open(staticCacheName)
     .then(function(cache) {
-			return cache.addAll(allCache);
+			return cache.addAll([
+        './',
+        './restaurant.html',
+        './index.html',
+        './js/restaurant_info.js',
+        './js/main.js',
+        './js/dbhelper.js',
+        './img/1.jpg',
+        './img/2.jpg',
+        './img/3.jpg',
+        './img/4.jpg',
+        './img/5.jpg',
+        './img/6.jpg',
+        './img/7.jpg',
+        './img/8.jpg',
+        './img/9.jpg',
+        './img/10.jpg',
+        './data/restaurants.json',
+        './css/styles.css',
+      ]);
     })
   );
 });
